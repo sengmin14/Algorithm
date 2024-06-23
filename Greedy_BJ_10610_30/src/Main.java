c5a5c80c65763faef88fc06d6f6bcf839d4079b4
진성민 <sengmin14@naver.com>
2024-06-23 22:01:00 +0900

    feat : 백준 10610

---
+++ Greedy_BJ_10610_30/src/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main
+{
+	static String str;
+	static long strLen;
+	static int[] numCountArr;
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		str = st.nextToken();
+		strLen = str.length();
+		numCountArr = new int[10];
+		long total = 0;
+		
+		for(int i = 0; i < strLen; i++)
+		{
+			int tNum = Integer.parseInt(str.substring(i, i+1));
+			numCountArr[tNum] += 1;
+			total += tNum;
+		}
+		
+		if(!str.contains("0") || total % 3 != 0)
+		{
+			System.out.println(-1);
+		}
+		else
+		{
+			StringBuilder sb = new StringBuilder();
+			for(int i = 9; i >= 0; i--)
+			{
+				while(numCountArr[i] > 0)
+				{
+					sb.append(i);
+					numCountArr[i]--;
+				}
+			}
+			System.out.println(sb.toString());
+		}
+	}
+}
---
+++ Test/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+class Main
+{
+	
+	static Queue<Integer> q1;
+	static List<Integer> q2;
+	
+	public static void main(String[] args)
+	{
+		q1 = new LinkedList<>();
+		q2 = new Stack<>();
+		
+		q2.add(3);
+		q2.add(5);
+		
+		
+	}
+}
