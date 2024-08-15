b6d0112c78a93aee8438bce7505fb980ed8bf7dc
진성민 <sengmin14@naver.com>
2024-08-15 22:09:24 +0900

    백준21608

--- Imple_BJ_21608_상어초등학교/src/Main.java
+++ Imple_BJ_21608_상어초등학교/src/Main.java
@@ -6,23 +6,22 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Seat implements Comparable<Seat>
+class Student implements Comparable<Student>
 {
 	int x;
 	int y;
 	int like;
 	int empty;
-	public Seat(int x, int y, int like, int empty)
+	public Student(int x, int y, int like, int empty)
 	{
-		super();
 		this.x = x;
 		this.y = y;
 		this.like = like;
 		this.empty = empty;
 	}
 	@Override
-	public int compareTo(Seat o)
-	{
+	public int compareTo(Student o) {
+		
 		if(this.like == o.like)
 		{
 			if(this.empty == o.empty)
@@ -45,86 +44,129 @@ class Seat implements Comparable<Seat>
 		{
 			return o.like - this.like;
 		}
+		
 	}
+	
+	
 }
 
 class Main
 {
-	static int[] di = {1, -1, 0, 0};
-	static int[] dj = {0, 0, 1, -1};
+	static int[] di = {-1, 0, 1, 0};
+	static int[] dj = {0, 1, 0, -1};
 	
 	static int N;
 	static int[][] map;
-	static int[] order;
-	static ArrayList[] list;
+	static ArrayList<Integer>[] list;
 	
-	int answer = 0;
+	static int[] sList;
 	
-	public int count(int x, int y)
-	{
-		int now = map[x][y];
-		int cnt = 0;
-		for(int i = 0; i < 4; i++)
+	static int answer;
+	
+	public int likeNo(int s, int x, int y)
+	{	
+		int sum = 0;
+		for(int k = 0; k < 4; k++)
 		{
-			int ni = x + di[i];
-			int nj = y + dj[i];
-			if(ni >= 1 && ni <= N && nj >= 0 && nj <= N)
+			int ni = x + di[k];
+			int nj = y + dj[k];
+			if(ni >= 0 && ni < N && nj >= 0 && nj < N)
 			{
-				if(list[now].contains(map[ni][nj]))
+				int check = map[ni][nj];
+				for(int i = 0; i < 4; i++)
 				{
-					cnt++;
+					if(list[s].get(i) == check)
+					{
+						sum++;
+					}
+				}
+			}
+		}
+		if(sum == 0)
+		{
+			return 0;
+		}
+		if(sum == 1)
+		{
+			return 1;
+		}
+		if(sum == 2)
+		{
+			return 10;
+		}
+		if(sum == 3)
+		{
+			return 100;
+		}
+		else
+		{
+			return 1000;
+		}
+	}
+	
+	public int countEmpty(int x, int y)
+	{
+		int sum = 0;
+		for(int k = 0; k < 4; k++)
+		{
+			int ni = x + di[k];
+			int nj = y + dj[k];
+			if(ni >= 0 && ni < N && nj >= 0 && nj < N)
+			{
+				if(map[ni][nj] == 0)
+				{
+					sum++;
 				}
 			}
 		}
 		
-		return cnt;
+		return sum;
 	}
 	
-	public int findLike(int x, int y, int no)
+	public int countLike(int start, int x, int y)
 	{
-		int like = 0;
-		for(int i = 0; i < 4; i++)
+		int sum = 0;
+		for(int k = 0; k < 4; k++)
 		{
-			int ni = x + di[i];
-			int nj = y + dj[i];
-			if(ni >= 1 && ni <= N  && nj >= 1 && nj <= N)
+			int ni = x + di[k];
+			int nj = y + dj[k];
+			if(ni >= 0 && ni < N && nj >= 0 && nj < N)
 			{
-				if(list[no].contains(map[ni][nj]))
-				like++;
-			}
-		}
-		return like;
-	}
-	
-	public int findSeat(int x, int y, int no)
-	{
-		int empty = 0;
-		for(int i = 0; i < 4; i++)
-		{
-			int ni = x + di[i];
-			int nj = y + dj[i];
-			if(ni >= 1 && ni <= N  && nj >= 1 && nj <= N)
-			{
-				if(map[ni][nj] == 0)
-				empty++;
-			}
-		}
-		return empty;
-	}
-	
-	public Seat solution(int no)
-	{
-		PriorityQueue<Seat> pq = new PriorityQueue<>();
-		for(int i = 1; i <= N; i++)
-		{
-			for(int j = 1; j <= N; j++)
-			{
-				if(map[i][j] == 0)
+				int check = map[ni][nj];
+				for(int i = 0; i < 4; i++)
 				{
-					pq.offer(new Seat(i, j, findLike(i, j, no), findSeat(i, j, no)));
+					if(list[start].get(i) == check)
+					{
+						sum++;
+					}
 				}
 			}
 		}
+		
+		return sum;
+	}
+	
+	public Student start(int s)
+	{
+		PriorityQueue<Student> pq = new PriorityQueue<>();
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				if(map[i][j] == 0)
+				{
+					int like = countLike(s, i, j);
+					int empty = countEmpty(i, j);
+					pq.add(new Student(i, j, like, empty));
+				}
+			}
+		}
+		
+//		while(!pq.isEmpty())
+//		{
+//			Student tmp = pq.poll();
+//			System.out.println(tmp.x + " " + tmp.y + " " + tmp.like + " " + tmp.empty);
+//		}
 		
 		return pq.poll();
 	}
@@ -138,49 +180,42 @@ class Main
 		
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		map = new int[N+1][N+1];
-		order = new int[N*N+1];
-		list = new ArrayList[N*N+1];
+		map = new int[N][N];
+		list = new ArrayList[(N*N)+1];
+		sList = new int[(N*N)+1];
 		
-		for(int i = 1; i < N*N+1; i++)
+		for(int i = 0; i <= N*N; i++)
 		{
 			list[i] = new ArrayList<>();
 		}
 		
-		for(int i = 1; i < N*N+1; i++)
+		for(int i = 1; i <= N*N; i++)
 		{
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			order[i] = a;
-			for(int j = 1; j < 5; j++)
+			int N = Integer.parseInt(st.nextToken());
+			sList[i] = N;
+			for(int k = 0; k < 4; k++)
 			{
-				int b = Integer.parseInt(st.nextToken());
-				list[a].add(b);
-			}			
-		}
-		for(int i = 1; i < N*N+1; i++)
-		{
-			// System.out.println(order[i]);
-			Seat now = T.solution(order[i]);
-			map[now.x][now.y] = order[i];
-		}
-//		for(int i = 1; i <= N; i++)
-//		{
-//			System.out.println(Arrays.toString(map[i]));
-//		}
-		
-		int sum = 0;
-		for(int i = 1; i <= N; i++)
-		{
-			for(int j = 1; j <= N; j++)
-			{
-				int count = T.count(i, j);
-				sum += Math.pow(10, count-1);
+				
+				int like = Integer.parseInt(st.nextToken());
+				list[N].add(like);
 			}
 		}
-		System.out.println(sum);
 		
+		for(int i = 1; i <= N*N; i++)
+		{
+			Student tmp = T.start(sList[i]);
+			map[tmp.x][tmp.y] = sList[i];
+		}
 		
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				answer += T.likeNo(map[i][j], i, j);
+			}
+		}
 		
+		System.out.println(answer);
 	}
 }
